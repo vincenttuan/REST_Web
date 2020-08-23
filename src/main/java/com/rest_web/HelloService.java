ef1adbf59a6c00e236aef929df14aa54dd0cc265
vincenttuan <vincentjava@yahoo.com.tw>
2020-08-23 22:22:02 +0800

    Java 8

--- src/main/java/com/rest_web/HelloService.java
+++ src/main/java/com/rest_web/HelloService.java
@@ -1,18 +1,10 @@
 package com.rest_web;
 
-import java.util.LinkedHashSet;
-import java.util.Optional;
-import java.util.Set;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
 
 @Path("hello")
 public class HelloService {
--- src/main/java/com/rest_web/MyApplication.java
+++ src/main/java/com/rest_web/MyApplication.java
@@ -1,13 +1,9 @@
 package com.rest_web;
 
-import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("v1")
 public class MyApplication extends Application {
-    @Override
-    public Set<Class<?>> getClasses() {
-        return super.getClasses();
-    }
+    
 }
