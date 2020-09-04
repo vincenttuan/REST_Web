931bebcd8ed770dde71315b048ef13509232b311
vincenttuan <vincentjava@yahoo.com.tw>
2020-09-04 14:40:18 +0800

    Java 8

--- src/main/java/com/jdbc_web/bean/ResultSetToBean.java
+++ src/main/java/com/jdbc_web/bean/ResultSetToBean.java
@@ -6,15 +6,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ResultSetToBean {
+
     /**
      * 根據一個 ResultSet 結果集(當前行)生一個成相應的數據實體
+     *
      * @param resultSet 結果集
-     * @param objectClass 數據實體類型,一個 JavaBean 如:  Book
+     * @param objectClass 數據實體類型,一個 JavaBean 如: Book
      * @param <T>
      * @return 封裝好的 Bean 物件
      * @throws SQLException
      */
-    public static<T> Object copy(ResultSet resultSet, Class<T> objectClass)  {
+    public static <T> Object copy(ResultSet resultSet, Class<T> objectClass) {
         // 通過反射創建 objectClass 物件
         Object obj = null;
         try {
@@ -30,22 +32,32 @@ public class ResultSetToBean {
             for (Field f : fields) {
                 //取出屬性名稱
                 System.out.println("屬性名稱: " + f);
-                String fieldName = f.toString().substring(f.toString().lastIndexOf(".")+1);
-                //判斷int類型
-                if (f.toString().indexOf("int") == -1) {
-                    invokeSet(obj, fieldName, resultSet.getString(fieldName));
-                } else {
+                String fieldName = f.toString().substring(f.toString().lastIndexOf(".") + 1);
+                //判斷類型
+                if (f.toString().indexOf("int") != -1) {
                     invokeSet(obj, fieldName, resultSet.getInt(fieldName));
+                } else if (f.toString().indexOf("Timestamp") != -1) {
+                    invokeSet(obj, fieldName, resultSet.getTimestamp(fieldName));
+                } else if (f.toString().indexOf("double") != -1) {
+                    invokeSet(obj, fieldName, resultSet.getDouble(fieldName));
+                } else if (f.toString().indexOf("float") != -1) {
+                    invokeSet(obj, fieldName, resultSet.getFloat(fieldName));
+                } else if (f.toString().indexOf("boolean") != -1) {
+                    invokeSet(obj, fieldName, resultSet.getBoolean(fieldName));
+                } else {
+                    invokeSet(obj, fieldName, resultSet.getString(fieldName));
                 }
+
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return obj;
     }
- 
+
     /**
      * 根據屬姓名獲取get方法
+     *
      * @param objectClass
      * @param fieldName 属性名
      * @return
@@ -67,22 +79,24 @@ public class ResultSetToBean {
         }
         return null;
     }
- 
+
     /**
      * 獲取所有屬性
+     *
      * @param objectClass
      * @return Field[]
      */
     private static Field[] getAllFieldName(Class objectClass) {
         Field[] fields = objectClass.getDeclaredFields();
-        for(Field f:fields){
+        for (Field f : fields) {
             f.setAccessible(true);
         }
         return fields;
     }
- 
+
     /**
      * 根據屬性名執行相對應得set方法
+     *
      * @param o 執行對象
      * @param fieldName 屬性名
      * @param value 屬性值
@@ -90,11 +104,10 @@ public class ResultSetToBean {
     private static void invokeSet(Object o, String fieldName, Object value) {
         Method method = getSetMethod(o.getClass(), fieldName);
         try {
-            method.invoke(o, new Object[] { value });
+            method.invoke(o, new Object[]{value});
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    
 }
